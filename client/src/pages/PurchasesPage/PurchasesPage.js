d38e91396b64c472c5069ea0d694adb93a073fef
oscarleonnogales <oscarleonnogales@gmail.com>
2021-04-21 12:53:44 -0700

    Pagination for purchase page

--- client/src/pages/PurchasesPage/PurchasesPage.js
+++ client/src/pages/PurchasesPage/PurchasesPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Purchase from '../components/Purchase/Purchase';
 import PurchaseForm from '../components/PurchaseForm/PurchaseForm';
 import Navbar from '../components/Navbar/Navbar';
@@ -7,20 +7,52 @@ import { useSelector } from 'react-redux';
 import './PurchasesPage.css';
 
 export default function PurchasesPage() {
-	const purchases = useSelector((state) => state.purchases);
+	const allPurchases = useSelector((state) => state.purchases);
+
+	const [currentPage, setCurrentPage] = useState(1);
+	const [startIndex, setStartIndex] = useState();
+	const [endIndex, setEndIndex] = useState();
+	const [currentPurchases, setCurrentPurchases] = useState({});
+
+	useEffect(() => {
+		setEndIndex(currentPage * 10);
+		setStartIndex((currentPage - 1) * 10);
+
+		setCurrentPurchases(allPurchases.slice(startIndex, endIndex));
+	}, [allPurchases, currentPage, endIndex, startIndex]);
+
+	function decreasePage() {
+		setCurrentPage((currentPage) => currentPage - 1);
+	}
+
+	function increasePage() {
+		setCurrentPage((currentPage) => currentPage + 1);
+	}
 
 	return (
 		<>
 			<Navbar></Navbar>
 			<main className="main-page-content">
 				<PurchaseForm />
-				{purchases.length > 0 && (
+				{currentPurchases.length > 0 && (
 					<div className="purchases-container">
-						{purchases.map((purchase) => {
+						{currentPurchases.map((purchase) => {
 							return <Purchase purchase={purchase} renderButtons={true} key={purchase._id} />;
 						})}
 					</div>
 				)}
+				<div className="pagination-btns-container">
+					{startIndex > 0 && (
+						<button className="pagination-btn" onClick={() => decreasePage()}>
+							Previous
+						</button>
+					)}
+					{endIndex < allPurchases.length && (
+						<button className="pagination-btn" onClick={() => increasePage()}>
+							Next
+						</button>
+					)}
+				</div>
 			</main>
 		</>
 	);
