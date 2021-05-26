7e0e74bc2da6b9ccf9d4d5b94c3be0e6148b8694
oscarleonnogales <oscarleonnogales@gmail.com>
2021-05-25 22:19:57 -0700

    Route to test DB connection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
 const router = express.Router();
+import Purchase from '../models/purchase.js';
 
 router.get('/', (req, res) => {
 	res.send('Hello from API');
+});
+
+router.get('/test-db-connection', async (req, res) => {
+	const randomPurchase = await Purchase.find().limit(1);
+	res.send(randomPurchase);
 });
 
 router.get('*', (req, res) => {
